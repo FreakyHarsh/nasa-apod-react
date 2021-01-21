7d5a13e058e9b22e2397a7c0c99b8f6cebffed91
FreakyHarsh <harsh.boricha2015@gmail.com>
2021-01-21 17:58:27 +0530

    feat: endpoint finished

--- Backend/README.md
+++ Backend/README.md
@@ -4,5 +4,7 @@ You can fetch the images using the given url and
 date format: `YYYY-MM-DD `
 
 ```
-GET: http://localhost:8080/image?date=2020-20-11
+GET: http://localhost:8080/image?date=2020-02-11
 ```
+
+For using this project: use your own [API KEY](https://api.nasa.gov/) and place it inside controllers/getImageByDate.js
--- Backend/controllers/getImageByDate.js
+++ Backend/controllers/getImageByDate.js
@@ -1,4 +1,12 @@
-exports.getImageByDate = (req, res, next) => {
-  console.log(req.query);
-  res.send(req.query);
+const fetch = require('node-fetch');
+
+exports.getImageByDate = async (req, res, next) => {
+  const date = req.query.date;
+  const response = await fetch(
+    `https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}&date=${date}`
+  )
+    .then((data) => data.json())
+    .then((data) => data);
+  console.log(response);
+  res.json(response);
 };
