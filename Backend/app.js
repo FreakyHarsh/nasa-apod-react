467be6610e1b4d8a56a325bf5b72104e8a70f033
FreakyHarsh <harsh.boricha2015@gmail.com>
2021-01-21 19:11:34 +0530

    feat: cors

--- Backend/app.js
+++ Backend/app.js
@@ -2,14 +2,19 @@ require('dotenv').config();
 const express = require('express');
 const helmet = require('helmet');
 const compression = require('compression');
+var cors = require('cors');
 
 const imageRoutes = require('./routes/image');
 
 const app = express();
 
+app.use(cors());
 app.use(helmet());
 app.use(compression());
 
 app.use(imageRoutes);
+app.use((req, res, next) => {
+  res.send('hello');
+});
 
 app.listen(8080);
