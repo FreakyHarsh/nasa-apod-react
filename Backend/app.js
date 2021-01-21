7f1a120adfd64c54ab309c5938a660d436e93f6d
FreakyHarsh <harsh.boricha2015@gmail.com>
2021-01-21 18:31:50 +0530

    feat: ready for deployment

--- Backend/app.js
+++ Backend/app.js
@@ -1,8 +1,14 @@
 require('dotenv').config();
 const express = require('express');
+const helmet = require('helmet');
+const compression = require('compression');
+
 const imageRoutes = require('./routes/image');
 
 const app = express();
+
+app.use(helmet());
+app.use(compression());
 
 app.use(imageRoutes);
 
